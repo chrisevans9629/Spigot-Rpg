7904c3a314f5022185d37cd84f82f596e5c167a9
Chris Evans <chrisevans9629@gmail.com>
2023-02-03 21:54:50 -0600

    improved farming

--- src/main/java/evans/rpg/plugin/listeners/PlantListener.java
+++ src/main/java/evans/rpg/plugin/listeners/PlantListener.java
@@ -6,11 +6,13 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Ageable;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -30,12 +32,7 @@ public class PlantListener implements Listener {
         if(multiPlant) {
             Block block = event.getBlock();
             if(block.getBlockData() instanceof Ageable){
-                //Ageable age = (Ageable) block.getBlockData();
-                //if(age.getAge() != age.getMaximumAge()){
-                //    logger.info("crop not ready... canceling");
-                //    event.setCancelled(true);
-                //    return;
-                //}
+
                 logger.info("broke a crop!!");
                 for (BlockFace face : BlockFace.values()){
                     Block relative = block.getRelative(face);
@@ -55,20 +52,42 @@ public class PlantListener implements Listener {
         }
     }
 
+    public int PlaceBlock(Block block, int stackCount, int count){
+        for (BlockFace face : BlockFace.values()){
+            Block relative = block.getRelative(face);
+            if(relative.canPlace(block.getBlockData()) && !(relative.getBlockData() instanceof Ageable) && count <= stackCount){
+                relative.setBlockData(block.getBlockData());
+                relative.setType(block.getType());
+                logger.info("placed block count: " + count);
+                count += 1;
+                count += PlaceBlock(relative, stackCount, count);
+            }
+        }
+
+        return count;
+    }
+
     @EventHandler
     public void onBlockPlaced(BlockPlaceEvent event){
-        boolean multiPlant = plugin.getClassFlag(event.getPlayer(), "plantMany");
+        Player player = event.getPlayer();
+        ItemStack stack = player.getItemInUse();
+
+        boolean multiPlant = plugin.getClassFlag(player, "plantMany");
         if(multiPlant){
 
             Block block = event.getBlockPlaced();
             if(block.getBlockData() instanceof Ageable){
+
                 logger.info("placed a crop!!");
-                for (BlockFace face : BlockFace.values()){
-                    Block relative = block.getRelative(face);
-                    if(relative.canPlace(block.getBlockData()) && !(relative.getBlockData() instanceof Ageable)){
-                        relative.setBlockData(block.getBlockData());
-                        relative.setType(block.getType());
-                    }
+                int stackCount = stack.getAmount();
+                int amountPlaced = PlaceBlock(block, stackCount, 0);
+                if(amountPlaced >= stackCount){
+                    logger.info("removing stack: " + stack.getType().name());
+                    player.getInventory().remove(stack);
+                }
+                else{
+                    logger.info("decreasing count to: " + amountPlaced);
+                    stack.setAmount(amountPlaced);
                 }
             }
         }
